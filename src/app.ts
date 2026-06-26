f401b4c332ea7acdc151f5a66c0c891c6b82da24
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-06-26 12:23:49 +0600

    Using Express Built In Middlwares In app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
 import express ,{ Application, Request, Response } from "express";
 
 const app : Application = express();
+import cors from 'cors';
+import config from "./config";
+import cookieParser from "cookie-parser";
+app.use(cors({
+    origin: config.app_url,
+    credentials:true,
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+
+
+
 app.get("/",(req:Request,res:Response)=>{
    res.send("Hello World");
 })
