805c6a56446cdfae74d81a513b430badbfedd1e0
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-06-30 11:58:30 +0600

    Defining Send Response Function

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -5,31 +5,8 @@ import httpStatus from "http-status";
 import config from "../../config";
 import { UserService } from "./user.service";
 import { catchAsync } from "../utils/catchAsync";
+import { sendResponse } from "../utils/sendResponse";
 
-type TMeta={
-  page:number;
-  limit:number;
-  total:number;
-}
-
-type TResponse<T>={
-  success:boolean;
-  statusCode:number;
-  message:string;
-  data:T;
-  meta?:TMeta;
-
-
-}
-const sendResponse=<T>(res:Response,data:TResponse<T>)=>{
-  res.status(data.statusCode).json({
-    success:data.success,
-    statusCode:data.statusCode,
-    message:data.message,
-    data:data.data,
-    meta:data.meta
-  })
-}
 
 const registerController=catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
   const payload=req.body;
