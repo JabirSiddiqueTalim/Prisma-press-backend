17c429020aa54aa5053aeadd9443e258361fd65b
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-06-30 11:54:15 +0600

    Create sendResponse

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -6,17 +6,48 @@ import config from "../../config";
 import { UserService } from "./user.service";
 import { catchAsync } from "../utils/catchAsync";
 
+type TMeta={
+  page:number;
+  limit:number;
+  total:number;
+}
+
+type TResponse<T>={
+  success:boolean;
+  statusCode:number;
+  message:string;
+  data:T;
+  meta?:TMeta;
+
+
+}
+const sendResponse=<T>(res:Response,data:TResponse<T>)=>{
+  res.status(data.statusCode).json({
+    success:data.success,
+    statusCode:data.statusCode,
+    message:data.message,
+    data:data.data,
+    meta:data.meta
+  })
+}
+
 const registerController=catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
   const payload=req.body;
   const user=await UserService.registerUserIntoDB(payload);
-  res.status(httpStatus.CREATED).json({
-      success:true,
-      statusCode:httpStatus.CREATED,
-      message:"User registered successfully",
-      data:{
-          user
-      }
+  // res.status(httpStatus.CREATED).json({
+  //     success:true,
+  //     statusCode:httpStatus.CREATED,
+  //     message:"User registered successfully",
+  //     data:{
+  //         user
+  //     }
 
+  // })
+  sendResponse(res,{
+    success:true,
+        statusCode:httpStatus.CREATED,
+        message:"User registered successfully",
+        data:{user}
   })
 })
 export const UserController={
