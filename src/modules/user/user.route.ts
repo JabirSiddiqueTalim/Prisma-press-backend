d032bca96c8eaa41f0007557b60e14ac24259a21
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-07-02 03:43:53 +0600

    Optimizing The Auth Middleware With Higher Order Function

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -6,6 +6,8 @@ import { prisma } from "../../lib/prisma";
 import httpStatus from "http-status";
 import { UserController } from "./user.controller";
 import { jwtUtils } from "../utils/jwt";
+import { catchAsync } from "../utils/catchAsync";
+import { JwtPayload } from "jsonwebtoken";
 const router =Router()
 declare global{
   namespace Express{
@@ -19,34 +21,87 @@ declare global{
     }
   }
 }
+const auth = (...requiredRole : UserRole[]) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.accessToken ? req.cookies.accessToken 
+    : req.headers.authorization?.startsWith("Bearer") ? req.headers.authorization.split(" ")[1] : 
+    req.headers.authorization
+
+    if (!token) {
+      throw new Error("You are not logged in");
+    }
+
+    const verifyToken = jwtUtils.varifiedToken(token, config.jwt_access_secret);
+
+    if (!verifyToken.success) {
+      throw new Error( verifyToken.error);
+    }
+    const { id, name, email, role } = verifyToken.data as JwtPayload;
+    console.log(role)
+
+    if (!requiredRole.includes(role)) {
+        throw new Error("You are not authorized to access this route");
+    }
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id,
+            email,
+            role,
+            name
+        }
+    })
+
+    if (!user) {
+        throw new Error("User not found");
+    }
+
+    if (user.activeStatus === "BLOCKED") {
+        throw new Error("Your account has beem blocked. Please contact with support team");
+    }
+
+    req.user = {
+      id,
+      name,
+      email,
+      role,
+    };
+
+    next();
+
+  });
+};
 
 router.post("/register",UserController.registerController)
-router.get("/me",(req:Request,res:Response,next:NextFunction)=>{
-  const {accessToken}=req.cookies;
-  console.log(accessToken)
-  const varifiedToken= jwtUtils.varifiedToken(accessToken,config.jwt_access_secret)
+router.get("/me",
+//   (req:Request,res:Response,next:NextFunction)=>{
+//   const {accessToken}=req.cookies;
+//   console.log(accessToken)
+//   const varifiedToken= jwtUtils.varifiedToken(accessToken,config.jwt_access_secret)
  
-  if(typeof varifiedToken === "string")
-  {
-    throw new Error(varifiedToken);
-  }
- const {email,name,id,role}=varifiedToken;
- const requiredRoles=[Role.ADMIN,Role.AUTHOR,Role.USER];
- if(!requiredRoles.includes(role))
- {
-  return res.status(403).json({
-    success:false,
-    statusCode:httpStatus.FORBIDDEN,
-    message:"Forbidden . you don't have permission to access this resource"
-  })
- }
- req.user={
-  email,
-  name,
-  id,
-  role
- }
-next()
+//   if(typeof varifiedToken === "string")
+//   {
+//     throw new Error(varifiedToken);
+//   }
+//  const {email,name,id,role}=varifiedToken.data as JwtPayload;
+//  const requiredRoles=[Role.ADMIN,Role.AUTHOR,Role.USER];
+//  if(!requiredRoles.includes(role))
+//  {
+//   return res.status(403).json({
+//     success:false,
+//     statusCode:httpStatus.FORBIDDEN,
+//     message:"Forbidden . you don't have permission to access this resource"
+//   })
+//  }
+//  req.user={
+//   email,
+//   name,
+//   id,
+//   role
+//  }
+// next()
 
-},UserController.getMyProfile)
+// },
+auth(Role.ADMIN,Role.AUTHOR,Role.USER),
+UserController.getMyProfile)
 export const userRouter=router;
--- src/modules/utils/jwt.ts
+++ src/modules/utils/jwt.ts
@@ -4,10 +4,22 @@ const createToken=(payload:JwtPayload,secret:string,expiresIn:SignOptions)=>{
   const jwtToken=jwt.sign(payload,secret,{expiresIn}as SignOptions)
   return jwtToken;
 }
-const varifiedToken=(accessToken:string,secret:string)=>{
-  const varifiedToken=jwt.verify(accessToken,secret);
-  return varifiedToken;
+const  varifiedToken = (token : string, secret : string) => {
+  try {
+       const varifiedToken = jwt.verify(token, secret);
+       return {
+           success: true,
+           data: varifiedToken
+       };
+  } catch (error : any) {
+       console.log("Token verification failed:", error);
+       return {
+           success: false,
+           error : error.message
+       }
+  }
 }
+
 export const jwtUtils={
   createToken,
   varifiedToken
