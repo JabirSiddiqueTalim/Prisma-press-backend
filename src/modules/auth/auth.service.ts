096057525cb9d175750b9250aa0fb6fd0c806e3a
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-07-01 10:44:04 +0600

    Implementing Jwt For Access Token And Refresh Token

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
 import { ILoginUser } from "./auth.interface";
+import  jwt, { SignOptions }  from "jsonwebtoken";
+import config from "../../config";
 
 const loginService=async (payload: ILoginUser)=>{
   const {email,password}=payload;
@@ -12,8 +14,26 @@ const loginService=async (payload: ILoginUser)=>{
   {
     throw new Error("Password is incorrect!!!")
   }
+  const jwtPayload={
+    id:user.id,
+    name:user.name,
+    email:user.email,
+    role:user.role
+  }
+  // const accessToken=Jwt.sign(jwtPayload,config.jwt_access_secret,config.jwt_access_expires_in as SignOptions
+  //  ) 
+  const accessToken=jwt.sign(jwtPayload,config.jwt_access_secret,{
+    expiresIn:config.jwt_access_expires_in
+  }as SignOptions)
 
-  return user;
+  const refreshToken=jwt.sign(jwtPayload,config.jwt_refresh_secret,{
+    expiresIn:config.jwt_refresh_expires_in
+  }as SignOptions)
+
+  return {
+    accessToken,
+    refreshToken
+  };
 }
 export const authService={
   loginService
